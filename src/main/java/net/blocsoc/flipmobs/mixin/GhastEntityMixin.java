b5b444cfb57472c9f026c305eafa98849755a2d9
Rory McDermid <2566302M@student.gla.ac.uk>
2025-01-09 14:41:53 +0000

    Ghasts now do nothing, this needs fixed

--- src/main/java/net/blocsoc/flipmobs/mixin/GhastEntityMixin.java
+++ src/main/java/net/blocsoc/flipmobs/mixin/GhastEntityMixin.java
@@ -2,6 +2,9 @@ package net.blocsoc.flipmobs.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.FlyingEntity;
 import net.minecraft.entity.mob.GhastEntity;
 import net.minecraft.entity.mob.HostileEntity;
@@ -14,6 +17,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Set;
+
 @Mixin(GhastEntity.class)
 public abstract class GhastEntityMixin extends FlyingEntity {
 
@@ -24,10 +29,15 @@ public abstract class GhastEntityMixin extends FlyingEntity {
 
 
 
-    @Inject(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/monster/GhastEntity$FlyRandomlyGoal;<init>(Lnet/minecraft/entity/monster/GhastEntity;)V", shift = At.Shift.AFTER), cancellable = true)
+
+
+
+    @Inject(method = "initGoals", at = @At("HEAD"), cancellable = true)
     protected void initGoals(CallbackInfo ci) {
         ci.cancel();
+
     }
+
 
     @Inject(method = "isDisallowedInPeaceful", at = @At("HEAD"), cancellable = true)
     protected boolean isDisallowedInPeaceful(CallbackInfoReturnable<Boolean> cir) {
